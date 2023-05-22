0b6a5157e6b2d28399bad79ee74d1c2b79b80e73
Julian Rodriguez <jrodriguezc@gmail.com>
2023-05-22 15:49:19 -0500

    [JRC-01] Using Java Annotations
    - Add configuration class
    - Create beans
    - Modify application

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -2,11 +2,15 @@ package com.pluralsight;
 
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
         System.out.println(service.findAll());
     }
 }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,10 +8,18 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository speakerRepository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository speakerRepository;
+
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository){
+        this.speakerRepository = speakerRepository;
+    }
 
     @Override
     public List<Speaker> findAll(){
         return speakerRepository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
     }
 }
