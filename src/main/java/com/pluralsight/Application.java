de8b42c595e73f4095756cc0e0ffea6e4f3305ec
Julian Rodriguez <jrodriguezc@gmail.com>
2023-05-22 21:55:40 -0500

    [JRC-03] Using XML
    - Add applicationContext.xml
    - Add different options for DI

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,12 +1,16 @@
 package com.pluralsight;
 
 import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
+
         System.out.println(service.findAll());
     }
 }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,17 +1,27 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository speakerRepository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository speakerRepository;
+
+    public SpeakerServiceImpl() {
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
+    }
 
     @Override
     public List<Speaker> findAll(){
         return speakerRepository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
     }
 }
